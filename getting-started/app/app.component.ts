b807b3d42c41d53162fa57b29c426e82995f8a1c
Jan Bevers <jbevers@sdl.com>
2016-10-14 12:23:28 +0200

    Hack results

--- getting-started/app/app.component.ts
+++ getting-started/app/app.component.ts
@@ -1,33 +1,54 @@
-import { Component } from '@angular/core';
-
-// Book Type
-export class Book {
-  title: Number;
-  cover: String;
-  category: String;
-}
-
-// TODO: Category Type
-export class Category {}
+import { Component, OnInit } from '@angular/core';
+import { Book, mockBooks } from './mocks/books';
+import { ICategory, categories } from './mocks/categories';
 
 @Component({
   selector: 'bookstore',
   // template: ``,
   templateUrl: '../app/app.template.html'
 })
-
-export class AppComponent {
-  books: Book [] = []; // use mocks data instead
-  categories: String [] = ['All', 'Web']; // use mocks data instead
+export class AppComponent implements OnInit {
+  books: Book[];
+  categories: ICategory[];
   navClosed: Boolean = true;
+  searchQuery: string = "";
+  private selectedCategory: ICategory
 
-  clicked() {
-    console.log('Will be implemented in the next section');
+  ngOnInit(): void {
+    this.books = mockBooks;
+    this.categories = categories;
+    // Not that clean as we make an assumption that the first item is the default one
+    this.selectedCategory = categories[0];
   }
 
-  search(){}
+  clicked(book: Book) {
+    console.log('Will be implemented in the next section', book);
+  }
 
-  toggleSideBar(){
+  onCategorySelected(category: ICategory) {
+    this.categories = categories.map(c => {
+      c.selected = category.name === c.name;
+      return c;
+    });
+
+    this.selectedCategory = category;
+    this.books = this.filterBooks();
+  }
+
+  search() {
+    this.books = this.filterBooks();
+  }
+
+  toggleSideBar() {
     this.navClosed = !this.navClosed;
+  }
+
+  private filterBooks(): Book[] {
+    let regex = new RegExp(`.*${this.searchQuery.toLowerCase().trim()}.*`, 'gi');
+    const category = this.selectedCategory;
+
+    return mockBooks.filter(book => {
+      return !!book.title.match(regex) && (category.name === "All" || book.category === category.name);
+    });
   }
 }
